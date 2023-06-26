dbe85788a3fd76df986185f38d83666f5487bc2a
Aaron Dean <adean30462@gmail.com>
2023-06-26 12:26:00 -0400

    chap4

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,19 @@
+﻿using DogGo.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+        Owner GetOwnerByEmail(string email);
+        void AddOwner(Owner owner);
+        void UpdateOwner(Owner owner);
+        void DeleteOwner(int id);
+
+    }
+}
